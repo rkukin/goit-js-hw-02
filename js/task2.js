d3145fa915dab4f2927ad16072132ccf1d4fb3b4
rkukin <romakukin@gmail.com>
2019-07-31 21:51:26 +0300

    task2

--- js/task2.js
+++ js/task2.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const names = ["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"];
+const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+
+function logItems(array) {
+  let counter = 1;
+  for (let i = 0; i < array.length; i++) {
+    console.log(`${counter} - ${array[i]}`);
+    counter++;
+  }
+}
+
+logItems(names);
+
+logItems(numbers);
