5cc9f8c9a3d2400669cffc0f546566c03173de06
rkukin <romakukin@gmail.com>
2019-07-31 22:52:37 +0300

    task4

--- js/task4.js
+++ js/task4.js
@@ -1,21 +1,21 @@
 "use strict";
 
 const findLongestWord = string => {
-  let longestIndex = 0;
+  let longestWord;
+  let longestWordLength = 0;
   const words = string.split(" ");
 
-  console.log(words);
-  for (let i = 0; i < words.length; i += 1) {
-    if (words[i].length > longestIndex) {
-      longestIndex = i;
-      console.log(words[i], words[longestIndex]);
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWordLength) {
+      longestWordLength = words[i].length;
+      longestWord = words[i];
     }
   }
-  return words[longestIndex];
+  return longestWord;
 };
 
-// console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
 
 console.log(findLongestWord("Google do a roll")); // 'Google'
 
-// console.log(findLongestWord("May the force be with you")); // 'force'
+console.log(findLongestWord("May the force be with you")); // 'force'
