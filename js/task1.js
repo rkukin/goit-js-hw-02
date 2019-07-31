caf5cad9c4d92f70ec02504ffcfd7e8e2672be1a
rkukin <romakukin@gmail.com>
2019-07-31 21:33:42 +0300

    task1

--- js/task1.js
+++ js/task1.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const users = ["Mango", "Poly", "Ajax", "Chelsey"];
+
+users.shift();
+console.log(users);
+
+users.pop();
+console.log(users);
+
+users.unshift("Lux");
+console.log(users);
+
+users.push("Jay", "Kiwi");
+console.log(users);
+
+const userToDelete = "Ajax";
+users.splice(users.indexOf(userToDelete), 1);
+console.log(users);
+
+const userToInsert = "Kong";
+const insertBefore = "Jay";
+users.splice(users.indexOf(insertBefore), 0, userToInsert);
+console.log(users);
