e9b0419719f57a3726079207e0199792d500cd60
rKukin <romakukin@gmail.com>
2019-08-01 15:40:59 +0300

    task6

--- js/task6.js
+++ js/task6.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const checkForSpam = string => {
+    let phrase = string.toLowerCase();
+
+        if (phrase.indexOf('sale') !== -1 || phrase.indexOf('spam') !== -1) {
+            return true;
+        }
+
+    return false;
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
